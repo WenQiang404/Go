826fcfc4dd97585920d23d4ab70f773d77986c4f
文强 <120762982+WenQiang404@users.noreply.github.com>
2023-03-08 16:09:00 +0800

    将server中跟用户相关的方法独立封装

--- srever/user.go
+++ srever/user.go
@@ -9,16 +9,18 @@ type User struct {
 	Addr    string
 	Channel chan string
 	conn    net.Conn
+	server  *Server
 }
 
 //创建一个用户的API
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User { //添加server参数，表示当前用户属于哪个server
 	userAddr := conn.RemoteAddr().String()
 	user := &User{
 		Name:    userAddr,
 		Addr:    userAddr,
 		Channel: make(chan string),
 		conn:    conn,
+		server:  server,
 	}
 
 	//启动监听channel的goroutine
@@ -33,4 +35,34 @@ func (this *User) ListenMessage() {
 
 		this.conn.Write([]byte(msg + "\n"))
 	}
+}
+
+//用户上线的业务
+func (this *User) Online() {
+	//用户上线
+	//因为要向OnlineMap中写入数据，所以需要先上锁
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	//广播用户上线的消息
+	this.server.Transfer(this, "the user has logging in")
+
+}
+
+//用户下线的业务
+func (this *User) Offline() {
+	//用户下线
+	//因为要向OnlineMap中删除数据，所以需要先上锁
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	//广播用户上线的消息
+	this.server.Transfer(this, "the user has logging out")
+}
+
+//用户处理消息
+func (this *User) DoMessage(msg string) {
+	this.server.Transfer(this, msg)
 }
